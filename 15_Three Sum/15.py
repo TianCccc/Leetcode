dfe670b65161804ca834bb7213594d3d719794ec
weichen66 <xutiancheng2018@outlook.com>
2020-09-19 18:58:47 -0400

    Create 15.py

---
+++ 15_Three Sum/15.py
@@ -0,0 +1,40 @@
+# Three Sum
+"""
+Input: nums = [-1, 0, 1, 2, -1, -4]
+Ouput:[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+"""
+def threeSum(nums):
+
+    n = len(nums)
+    result = []
+    nums.sort()
+    
+    for i in range(n-2):
+        if nums[i] + nums[i+1] + nums[i+2] > 0:
+            break
+        if nums[i] + nums[n-2] + nums[n-1] < 0:
+            continue
+        # 重复值
+        if i > 0  and nums[i] == nums[i-1]:
+            continue
+        l, r = i+1, n-1
+
+        while l < r:
+            tmp = nums[i] + nums[l] + nums[r]
+            if tmp == 0:
+                result.append(nums[i], nums[l], nums[r])
+                while l+1 < r and nums[l] == nums[l+1]:
+                    l += 1
+                l += 1
+                while r < r-1 and nums[r] == nums[r-1]:
+                    r -= 1
+                r -= 1
+            elif tmp < 0:
+                l += 1
+            else: 
+                r -= 1
+    
+    return result
