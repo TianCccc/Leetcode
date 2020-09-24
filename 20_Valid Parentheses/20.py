8e644366d66e5564f7da7a3b03046310de271107
weichen66 <xutiancheng2018@outlook.com>
2020-09-24 19:11:25 -0400

    Create 20.py

---
+++ 20_Valid Parentheses/20.py
@@ -0,0 +1,30 @@
+# Valid Parentheses
+"""
+Input: s = "()"
+Output: true
+
+Input: s = "{[]}"
+Output: true
+
+Input: s = "([)]"
+Output: false
+"""
+
+def isValid(s):
+    d = {')': '(',
+         ']': '[',
+         '}': '{'}
+    stack = []
+    for i in s:
+        if (len(stack) != 0) and i in d:
+            # stack last element compared with element i
+            if stack[-1] == d[i]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(i)
+    
+    return not stack
+
+print(isValid("()"))
---
+++ 20_Valid Parentheses/README.md
@@ -0,0 +1,15 @@
+# Note
+ _s1_ 
+```diff
+! `stack`
+if (incoming not in dict):
+    append
+else (incoming in dict):
+    compare:
+        if incoming == stact[-1]:
+            pop
+        else:
+            Fales
+
+return not stack (if stack is null: True)
+```
